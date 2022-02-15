f16508b395026e2ed9f7b4ea288cdbf72f49214d
bhavstae <99749053+bhavstae@users.noreply.github.com>
2022-02-15 23:41:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var n= document.getElementById("No. of years").value;
      Compute Interest = (p*r*n)/100;
     result= document.getElementById("Compute Interest");
-    result.innerHTML="If you deposit + P, at an interest rate of + r. You will receive an amount of + result, in the year + No. of years";
+    result.innerHTML="If you deposit +(p), at an interest rate of +(r). You will receive an amount of +(result), in the year +(No. of years)";
     
     
 }
