dd438d480b733776473d3997e593cae1083b58df
bhavstae <99749053+bhavstae@users.noreply.github.com>
2022-02-16 00:47:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function compute()
 {
     var p = document.getElementById("Amount").value;
-   float r = document.getElementById("Interest rate").value;
+    if(parseInt(p) < 1) {
+		alert("Enter a positive number");
+		document.getElementById("p").focus();
+		return;
+	}
+   var r = document.getElementById("Interest rate").value;
     var n= document.getElementById("No. of years").value;
      Compute Interest = (p*r*n)/100;
+     var FY = new Date().getFullYear() + n;
     result= document.getElementById("Compute Interest");
-    result.innerHTML="If you deposit +(p), at an interest rate of +(r). You will receive an amount of +(result), in the year +(No. of years)";
-   
+    result.innerHTML="<br/> result : If you deposit  <mark>" + p + "</mark>,<br/>" +
+        "at an interest rate of  <mark>" + r + "</mark>,<br/><br/>" +
+        "You will receive an amount of  <mark>" + Compute Interest + "</mark>,<br/> " +
+        "in the year <mark>" + FY + "</mark><br/>";
 }
         
