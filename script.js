7bc715e6d608c916b26d916fa667312384e1117a
bhavstae <99749053+bhavstae@users.noreply.github.com>
2022-02-16 21:43:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function showslider(x) {
 }
 function compute()
 {
-    var p = parseFloat(document.getElementById("principal").value);
-   var r = parseFloat(document.getElementById("ratespan").value);
-    var n= parseFloat(document.getElementById("Years").value);
+    var p = parseFloat(document.getElementById("principal").value,10);
+   var r = parseFloat(document.getElementById("ratespan").value,10);
+    var n= parseFloat(document.getElementById("Years").value,10);
      var Interest = p*r*n/100;
      var FY = new Date().getFullYear() + Years;
     document.getElementById("result").innerHTML="<br />If you deposit <mark>"+principal+"</mark>,<br /> at an interest rate of <mark>"+ratespan+"%</mark>.
