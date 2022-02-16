6431900440e5c945f780290339b8431c53d4a515
bhavstae <99749053+bhavstae@users.noreply.github.com>
2022-02-16 15:26:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function compute()
 {
-    var p = document.getElementById("Amount").value;
-    if(parseInt(p) < 1) {
+    var p = parseInt(document.getElementById("Amount").value);
+    if(p < 1) {
 		alert("Enter a positive number");
 		document.getElementById("p").focus();
 		return;
 	}
-   var r = document.getElementById("Interest rate").value;
-    var n= document.getElementById("No. of years").value;
+   var r = parseFloat(document.getElementById("Interest rate").value);
+    var n= parseInt(document.getElementById("No. of years").value);
      Compute Interest = (p*r*n)/100;
      var FY = new Date().getFullYear() + n;
     result= document.getElementById("Compute Interest");
-    result.innerHTML="<br/> result : If you deposit  <mark>" + p + "</mark>,<br/>" +
+    result.innerHTML="<br/> Interest : If you deposit  <mark>" + p + "</mark>,<br/>" +
         "at an interest rate of  <mark>" + r + "</mark>,<br/><br/>" +
         "You will receive an amount of  <mark>" + Compute Interest + "</mark>,<br/> " +
-        "in the year <mark>" + FY + "</mark><br/>";
+        "in the year <mark>" + FY + "</mark><br/>";	
 }
-        
+        function SliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
